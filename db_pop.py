9f4259d348a28d6859a1a639cf8f28e316dbe79d
phoughton <pete@investigatingsoftware.co.uk>
2023-07-22 17:59:02 +0000

    moved to new folders and working but slow

--- db_pop.py
+++ db_pop.py
@@ -2,6 +2,7 @@ import chromadb
 import PyPDF2
 import os
 import json
+from chromadb.config import Settings
 
 
 def extract_text_from_pdf(file_path: str, pdf_file_name: str) -> list:
@@ -11,53 +12,86 @@ def extract_text_from_pdf(file_path: str, pdf_file_name: str) -> list:
 
     for num, page in enumerate(pdf_reader.pages):
         text.append({"id": f"{pdf_file_name}: page {num + 1}",
-                    "page_of_text": page.extract_text()})
+                    "piece_of_text": page.extract_text()})
 
     pdf_file_obj.close()
 
     return text
 
 
-def read_all_pdfs(pdf_folder: str) -> list:
-    pdf_files = os.listdir(pdf_folder)
+def extract_text_from_txt_file(file_path: str, txt_file_name: str) -> list:
+    txt_file_obj = open(file_path, 'r')
+    text = []
+
+    # split file into paragraphs
+    paragraphs = txt_file_obj.read().split("\n\n")
+
+    for num, paragraph in enumerate(paragraphs):
+        text.append({"id": f"{txt_file_name}: paragraph {num + 1}",
+                    "piece_of_text": paragraph})
+
+    txt_file_obj.close()
+
+    return text
+
+
+def read_all_files(folder_path: str, 
+                   file_extension: str, 
+                   extract_func) -> list:
+
+    files = os.listdir(folder_path)
     books_n_text = []
 
-    for pdf_file in pdf_files:
-        if pdf_file.endswith(".pdf"):
-            pdf_path = os.path.join(pdf_folder, pdf_file)
-            text = extract_text_from_pdf(pdf_path, pdf_file)
+    for file_name in files:
+        if file_name.endswith(file_extension):
+            print(file_name)
+            file_path = os.path.join(folder_path, file_name)
+            text = extract_func(file_path, file_name)
             books_n_text += (text)
 
     return books_n_text
 
 
-books_and_text = read_all_pdfs("data/")
+books_and_text = read_all_files("data_in/bank_of_england",
+                                ".pdf", 
+                                extract_text_from_pdf)
+books_and_text += read_all_files("data_in/charles_dickens",
+                                 ".txt", 
+                                 extract_text_from_txt_file)
 
 ids = []
-for page in books_and_text:
-    ids.append(page["id"])
+for text_chunk_details in books_and_text:
+    ids.append(text_chunk_details["id"])
 
-document_pages = []
-for page in books_and_text:
-    document_pages.append(page["page_of_text"])
+document_pieces = []
+for text_chunk_details in books_and_text:
+    document_pieces.append(text_chunk_details["piece_of_text"])
 
-chroma_client = chromadb.Client()
+print()
 
-collection = chroma_client.create_collection(name="test_collection")
+chroma_client = chromadb.Client(Settings(
+    chroma_db_impl="duckdb+parquet",
+    persist_directory="data_store/vector_db"))
 
+collection = chroma_client.create_collection(name="book_collection")
+
+with open("data_store/books_and_text.json", "w") as f:
+    json.dump(books_and_text, f, indent=4)
 
 book_names_metadata = []
 for id in ids:
     book_names_metadata.append({id.split(":")[0]: id.split(":")[1]})
 
-collection.add(
-    documents=document_pages,
-    metadatas=book_names_metadata,
-    ids=ids
-    )
+for counter in range(0, len(book_names_metadata)):
+    print(ids[counter])
+    collection.add(
+        documents=document_pieces[counter],
+        metadatas=book_names_metadata[counter],
+        ids=ids[counter]
+        )
 
 results = collection.query(
-    query_texts=["money flow"],
+    query_texts=["guilty", "innocent"],
     n_results=2
 )
 
