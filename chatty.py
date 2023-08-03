2e240db4a390f1344a15d529f8a94a6ce3f2c3a4
phoughton <pete@investigatingsoftware.co.uk>
2023-08-03 19:18:15 +0000

    base system prompt

--- chatty.py
+++ chatty.py
@@ -3,3 +3,14 @@ import openai
 import requests
 
 
+
+openai.api_key = config("API_KEY")
+THE_SCORER_URL = str(config("SCORING_URL"))
+
+message_flow = [
+    {
+        "role": "system", "content": """
+You are an expert on Charles Dickens, and you are talking to a student who is studying English literature.
+You will reply politely and informative to the student's questions.
+"""}
+]
